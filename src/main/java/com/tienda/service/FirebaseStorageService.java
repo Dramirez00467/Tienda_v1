541c76d81a8dcb8d83158011040952b77e67f93d
Dramirez00467 <106043587+Dramirez00467@users.noreply.github.com>
2024-07-04 19:42:06 -0600

    Clase 6 Firebase

---
+++ src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,21 @@
+package com.tienda.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-5967e.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-5967e-firebase-adminsdk-2a4rl-0e060e5647.json";
+
+}
--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,4 +1,4 @@
-package com.tienda.service.service.impl;
+package com.tienda.service.impl;
 
 import com.tienda.dao.CategoriaDao;
 import com.tienda.domain.Categoria;
